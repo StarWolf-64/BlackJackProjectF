e33724660db9b258d130e10c02adf8cece0ef630
bpottsUMW <bpotts@mail.umw.edu>
2022-11-26 16:09:25 -0500

    game checks players scores to end game

--- blackjack.c
+++ blackjack.c
@@ -19,6 +19,40 @@ int main(int argc, char **argv){
 	initCard(); // set up unicode printing
 	initDeck(deck);// makes the memory for the deck
 	shuffleDeck(shuffled, deck); //shuffledeck shuffles the deck
-	printf("make file works\n");
+	playersInit();
+	printf("Game start\n");
+
+	//game loop ending conditions
+	int gamego = 1;
+	int playersuse = 5;
+	int loopcount = 0;
+
+	//main game loop
+	while(gamego){
+		
+		//test code
+		for(int i = 1; i < playersuse; i++){
+			//printf("player %d score = %d\n", i, players[i].score);
+			players[i].score = players[i].score - 50;
+			//printf("player %d new score = %d\n", i, players[i].score);
+		}
+
+		//get sum of all player scores
+		int scoresum = 0;//
+		for(int i = 1; i < playersuse; i++){
+			scoresum += players[i].score;
+		}
+
+		//end while loop check
+		int endcheck = 10 * playersuse;
+		if(scoresum <  endcheck){
+			gamego = 0;
+		}
+		
+		//test code
+		printf("loop iteration %d\n", loopcount);
+		loopcount++;
+
+	}
 
 }
